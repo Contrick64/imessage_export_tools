5e9a824b5c4443f285889548fadd5dec5cc6b4ee
Connor Murphy <connor@murphysites.com>
2023-11-13 19:15:54 -0500

    fiddle around with encoding... find nothing

--- organize_imessage_conversations.py
+++ organize_imessage_conversations.py
@@ -3,5 +3,5 @@ import os
 dir_path = '../test_messages'
 
 for filename in os.listdir(dir_path) :
-    with open(dir_path + '/' + filename) as f:
-        print(f.readline())
+    with open(dir_path + '/' + filename,'r',encoding='utf-8') as f: #weird encoding in these files, will investigate further
+        contents = f.read()
