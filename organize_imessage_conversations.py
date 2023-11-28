00935cfaf1c8a8552c130741a94cda9c56f8e683
Connor Murphy <connor@murphysites.com>
2023-11-27 23:25:56 -0500

    define class Chat_Log based on filename

--- organize_imessage_conversations.py
+++ organize_imessage_conversations.py
@@ -1,7 +1,41 @@
 import os
+import datetime
+from re import search,findall
+from glob import iglob
 
-dir_path = '../test_messages'
+dir_path = os.path.realpath(os.path.join(os.path.dirname(__file__),'../test_messages/'))
 
-for filename in os.listdir(dir_path) :
-    with open(dir_path + '/' + filename,'r',encoding='utf-8') as f: #weird encoding in these files, will investigate further
-        contents = f.read()
+class Chat_Log:
+    def __init__(self,filename):
+        date_re = '(\d{4})-(\d\d)-(\d\d)'
+        time_re = '(\d\d)\.(\d\d)\.(\d\d)'
+        regex = f'^(?:Chat with )?(.*?)(?: et al)? on {date_re} at {time_re}\.txt'
+        self.isGroupChat = bool(search("^Chat with .*? et al",filename))
+        result = findall(regex,filename)[0]
+        self.contact = result[0]
+        dt_tuple = []
+        for i in result[1:]:
+            dt_tuple.append(int(i))
+
+        self.datetime = datetime.datetime(*dt_tuple)
+    
+    def __repr__(self):
+        c = self.contact
+        d = self.datetime
+        g = self.isGroupChat
+        return f"<Chat_Log contact:{c} datetime:{d} group:{g}>"
+
+    def __str__(self):
+        c="C"
+        if self.isGroupChat:
+            c="Group c"
+        return f"{c}hat with {self.contact} at {self.datetime}"
+
+chat_logs = []
+
+for filepath in iglob(dir_path + '**/**', recursive=True):
+    filename = filepath.split('/')[-1]
+    if filename:
+        new_log = Chat_Log(filename)
+        chat_logs.append(new_log)
+        print(new_log)
