1c17c5a3f106d682b180e9beec79b52b0c3b364b
Connor Murphy <connor@murphysites.com>
2024-06-25 17:19:07 -0400

    add new parser class

--- organize_imessage_conversations.py
+++ organize_imessage_conversations.py
@@ -3,35 +3,40 @@ import datetime
 from re import search,findall
 from shutil import copy as shutil_copy
 
-class Chat_Log:
+# CHANGE THESE TO THE CORRECT FULL PATHS 
+input_dir_path = '../jf_imessages'
+output_dir_path = '../jf_imessages_sorted'
+
+class Bornstein_Chat_Log:
     def __init__(self,filename):
-        group_sort = False
+        group_sort = True
 
         phone_dir = 'misc_phone_numbers'
         group_chat_dir = 'misc_group_chats'
         date_re = r'(\d{4})-(\d\d)-(\d\d)'
-        time_re = r'(\d\d)\.(\d\d)\.(\d\d)'
-        phone_re = r'(?:\+1 ?)?\(?(?:[0-9]{3})\)?[-.\s]?(?:[0-9]{3})[-.\s]?(?:[0-9]{4})'
-        regex = r'^(?:Chat with )?(.?'+phone_re+r'.?|.*?)(?: et al)? on '+date_re+r' at '+time_re+r'\.txt'
+        time_re = r'(\d\d)\.(\d\d)(?:\.(\d\d))?'
+        phone_re = r'^.{0,2}[\+\d]\d+'
+        regex = r'^(?:Chat with )?(.*?)(?: et al)? on '+date_re+r' at '+time_re+r'(?:-\d+)?\.txt'
         self.isGroupChat = bool(search("^Chat with .*? et al",filename))
         reg_match = findall(regex,filename)
         if not len(reg_match):
             return
         result = reg_match[0]
-        self.contact = result[0]
+        self.contact = str(result[0])
         dt_tuple = []
         for i in result[1:]:
+            i = i if i else 0
             dt_tuple.append(int(i))
 
         self.datetime = datetime.datetime(*dt_tuple)
         if self.isGroupChat and group_sort:
             self.sort_as = group_chat_dir
-        if not search(phone_re,self.contact):
+        elif not search(phone_re,self.contact):
             self.sort_as = self.contact
         else:
             self.sort_as = phone_dir
     def __repr__(self):
-        return "<Chat_Log contact:%c datetime:%d group:%g>" % (
+        return "<Chat_Log contact:{} datetime:{} group:{}>".format(
             self.contact,
             self.datetime,
             self.isGroupChat
@@ -42,19 +47,48 @@ class Chat_Log:
         if self.isGroupChat:
             c="Group c"
         return f"{c}hat with {self.contact} at {self.datetime} ({self.sort_as})"
+    
+class Fernandez_Chat_Log:
+    def __init__(self, filename):
+        self.misc_phone_dir = 'misc_phone_numbers'
+        self.misc_group_dir = 'misc_group_chats'
+        self.phone_re = r'^.{0,2}[\+\d]\d+'
+        self.regex = r'Messages - (.*?&?).txt'
+        self.isGroupChat = bool(search("&",filename))
+        self.reg_match = findall(self.regex,filename)
+        if not len(self.reg_match):
+            return
+        result = self.reg_match[0]
+        self.contacts = [contact.strip() for contact in result.split('&')]
+        self.first_contact = self.find_first_contact()
+        if self.first_contact:
+            self.sort_as = self.first_contact
+            return
+        if self.isGroupChat:
+            self.sort_as = self.misc_group_dir
+        else:
+            self.sort_as = self.misc_phone_dir
+    
+    def find_first_contact(self):
+        for contact in self.contacts:
+            if not search(self.phone_re,contact):
+                return contact
+        return False
+
 
 if __name__ == "__main__":
-    parent_dir = Path(__file__).parent.parent
-    # dir_path = Path(parent_dir).joinpath('test_messages')
-    dir_path = Path(parent_dir).joinpath('20230526_Messages Folder_Access')
+    dir_path = Path(input_dir_path)
+    export_dir = Path(output_dir_path)
     for filepath in dir_path.glob("**/*"):
         filename = filepath.name
         if filename:
-            new_log = Chat_Log(filename)
-            if not hasattr(new_log,'contact'):
-                continue
-            contact_dir = parent_dir.joinpath('exports',new_log.sort_as)
+            new_log = Fernandez_Chat_Log(filename)
+            contact_dir = export_dir.joinpath(new_log.sort_as)
+
             if not contact_dir.is_dir():
-                contact_dir.mkdir()
+                contact_dir.mkdir(parents=True)
             shutil_copy(filepath,contact_dir.joinpath(filename))
 
+    for dir in export_dir.glob("*"):
+        file_count = len(list(dir.glob("*")))
+        print(f"{dir.stem}, {file_count}")
